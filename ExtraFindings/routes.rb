caa0f39da13ff03f122c6085468d0730fc702e83
nikosev <nikos.ev@hotmail.com>
2020-05-01 18:19:37 +0300

    Support importing findings in multiple languages

--- ExtraFindings/routes.rb
+++ ExtraFindings/routes.rb
@@ -38,7 +38,9 @@ post '/ExtraFindings/import' do
 	end
 	if params["BurpSuite"]
 		import_burpsuite
-		options.finding_types.push("BurpSuite")
+		if !(options.finding_types.include? 'BurpSuite')
+			options.finding_types.push("BurpSuite")
+		end
 	end
 	@success = "Imported findings"
 	haml :"../plugins/ExtraFindings/views/import"
@@ -111,69 +113,73 @@ end
 
 def import_burpsuite()
 	# Iterate the VulnDB database
-	burpsuite_dir = "#{Dir.pwd()}/plugins/ExtraFindings/data/BurpSuite/db/"
-	Dir.entries(burpsuite_dir).each do |json_file|
-		next if json_file == "." or json_file == ".."
+	burpsuite_dir = "#{Dir.pwd()}/plugins/ExtraFindings/data/BurpSuite/db/*/"
+	Dir[burpsuite_dir].each do |lang_dir|
+		Dir.entries(lang_dir).each do |json_file|
+			next if json_file == "." or json_file == ".."
 
-		# Read in the JSON file and store as json obj
-		file = File.read(burpsuite_dir+json_file)
-		json_data = JSON.parse(file)
+			# Read in the JSON file and store as json obj
+			file = File.read(lang_dir+json_file)
+			json_data = JSON.parse(file)
 
-		#### Change this portion if the VulnDB Schema changes
-		finding = {}
-		puts "|+| Importing #{json_data["title"]}"
-		finding["title"] = c(json_data["title"])
+			#### Change this portion if the VulnDB Schema changes
+			finding = {}
+			puts "|+| Importing #{json_data["title"]}"
+			finding["title"] = c(json_data["title"])
 
-		if json_data["description"].kind_of?(Array)
-			finding["overview"] = ""
-			json_data["description"].each do |rem|
-				finding["overview"] += "<paragraph>"
-				finding["overview"] += c(rem)
+			if json_data["description"].kind_of?(Array)
+				finding["overview"] = ""
+				json_data["description"].each do |rem|
+					finding["overview"] += "<paragraph>"
+					finding["overview"] += c(rem)
+					finding["overview"] += "</paragraph>"
+				end
+			else
+				finding["overview"] = "<paragraph>"
+				finding["overview"] += c(json_data["description"])
 				finding["overview"] += "</paragraph>"
 			end
-		else
-			finding["overview"] = "<paragraph>"
-			finding["overview"] += c(json_data["description"])
-			finding["overview"] += "</paragraph>"
-		end
 
-		if json_data["fix"]["guidance"].kind_of?(Array)
-			finding["remediation"] = ""
-			json_data["fix"]["guidance"].each do |rem|
-				finding["remediation"] += "<paragraph>"
-				finding["remediation"] += c(rem)
-				finding["remediation"] += "</paragraph>"
+			if json_data["fix"]["guidance"].kind_of?(Array)
+				finding["remediation"] = ""
+				json_data["fix"]["guidance"].each do |rem|
+					finding["remediation"] += "<paragraph>"
+					finding["remediation"] += c(rem)
+					finding["remediation"] += "</paragraph>"
+				end
+			else
+				finding["remediation"] = c(json_data["fix"]["guidance"])
 			end
-		else
-			finding["remediation"] = c(json_data["fix"]["guidance"])
-		end
 
-		finding["references"] = ""
-		if json_data["references"] != nil
-			json_data["references"].each do |ref|
-				finding["references"] += "<paragraph>"+c(ref["url"])+"</paragraph>"
+			finding["references"] = ""
+			if json_data["references"] != nil
+				json_data["references"].each do |ref|
+					finding["references"] += "<paragraph>"+c(ref["url"])+"</paragraph>"
+				end
 			end
+
+			finding["type"] = "BurpSuite"
+			finding["approved"] = true
+
+			finding["risk"] = 0 if json_data["severity"] == "Information"
+			finding["risk"] = 1 if json_data["severity"] == "Low"
+			finding["risk"] = 2 if json_data["severity"] == "Medium"
+			finding["risk"] = 3 if json_data["severity"] == "High"
+
+			# TODO: add a true DREAD score calculator
+			finding["damage"] = 1
+			finding["reproducability"] = 1
+			finding["exploitability"] = 1
+			finding["affected_users"] = 1
+			finding["discoverability"] = 1
+			finding["dread_total"] = 5
+			####
+
+			finding["language"] = c(json_data["language"])
+
+			# write the database
+			finding_db = TemplateFindings.create(finding)
+			finding_db.save
 		end
-
-		finding["type"] = "BurpSuite"
-		finding["approved"] = true
-
-		finding["risk"] = 0 if json_data["severity"] == "Information"
-		finding["risk"] = 1 if json_data["severity"] == "Low"
-		finding["risk"] = 2 if json_data["severity"] == "Medium"
-		finding["risk"] = 3 if json_data["severity"] == "High"
-
-		# TODO: add a true DREAD score calculator
-		finding["damage"] = 1
-		finding["reproducability"] = 1
-		finding["exploitability"] = 1
-		finding["affected_users"] = 1
-		finding["discoverability"] = 1
-		finding["dread_total"] = 5
-		####
-
-		# write the database
-	    finding_db = TemplateFindings.create(finding)
-	    finding_db.save
 	end
 end
